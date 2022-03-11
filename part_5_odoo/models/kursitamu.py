9eb74ad9dfbf51bd8f268f3a45dcd28292ffbf14
Yamikaze12 <59038986+Yamikaze12@users.noreply.github.com>
2022-03-11 20:32:44 +0700

    Fisrt Commit
    Part %

---
+++ part_5_odoo/models/kursitamu.py
@@ -0,0 +1,13 @@
+from odoo import api, fields, models
+
+
+class KursiTamu(models.Model):
+    _name = 'wedding.kursitamu'
+    _description = 'Daftar Kursi Tamu'
+
+    name = fields.Char(string='Name')
+    tipe = fields.Selection(string='Kursi Tamu', selection=[('plastik', 'Plastik'), ('stainless', 'Stainless'),])
+    stok = fields.Char(string='stok kursi')
+    harga = fields.Integer(string='Harga Sewa perunit')
+    
+    
